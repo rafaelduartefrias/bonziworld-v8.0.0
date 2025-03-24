06094e15327ba3a7a1161be189e4fa3fb9b2fae4
rafaelduartefrias <158203567+rafaelduartefrias@users.noreply.github.com>
2025-03-24 16:17:30 -0300

    Update script.js

--- cilent/script.js
+++ cilent/script.js
@@ -1,3 +1,4 @@
+let template = document.getElementById("welcome-template");
 let welcomeBodyContent = template.content.cloneNode(true);
 let mainContent = document.querySelector("#main-content");
 mainContent.innerHTML = "";
